97c2f32fb5dcf5286732f453931148fc2b6151ea
JSIT-20 <jdeme051@uottawa.ca>
2020-12-11 19:58:37 -0500

    init

---
+++ bot.js
@@ -0,0 +1,17 @@
+// Run dotenv
+require('dotenv').config();
+
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if (msg.member.user.username === 'zlafreniere1') {
+    msg.reply('g$');
+  }
+});
+
+client.login(process.env.DISCORD_TOKEN);
